59339641b3233b0d2f5a94a3b90563a458617b73
Jonas Neumayer <jonas.neumayer@web.de>
2025-11-04 16:03:43 +0100

    class 'Task' implemented

---
+++ src/task-manager-cli/core/task.py
@@ -0,0 +1,16 @@
+
+
+class Task:
+    def __init__(self, description: str, category: str, completed: bool):
+        self.description = description
+        self.category = category
+        self.completed = completed
+    
+    def update_description(self, new_description: str):
+        self.description = new_description
+    
+    def update_category(self, new_category: str):
+        self.category = new_category
+
+    def mark_completed(self):
+        self.completed = True
