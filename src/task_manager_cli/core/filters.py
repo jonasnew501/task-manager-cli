f741f5686dd8c7b6445887b2af043a5e4c5979c2
Jonas Neumayer <jonas.neumayer@web.de>
2025-11-06 15:11:33 +0100

    CategoryFilter and CompletedFilter implemented

--- src/task_manager_cli/core/filters.py
+++ src/task_manager_cli/core/filters.py
@@ -1,6 +1,21 @@
 from abc import ABC, abstractmethod
 
+from src.task_manager_cli.core.task import Task
+
 class TaskFilter(ABC):
     @abstractmethod
-    def match(self, task) -> bool:
+    def match(self, task: Task) -> bool:
         pass
+
+
+class CategoryFilter(TaskFilter):
+    def __init__(self, category: str) -> None:
+        self.category = category
+    
+    def match(self, task: Task) -> bool:
+        return task.category == self.category
+
+class CompletedFiler(TaskFilter):
+    def match(self, task: Task) -> bool:
+        return task.completed
+        
