0cea8ea1d2d20d0b86f1a5e25b16822c5f86cdd9
Jonas Neumayer <jonas.neumayer@web.de>
2025-11-06 15:32:15 +0100

    Showcase of a violation of the Open-Closed-Principle added to 'filters.py'

--- src/task_manager_cli/core/filters.py
+++ src/task_manager_cli/core/filters.py
@@ -18,4 +18,22 @@ class CategoryFilter(TaskFilter):
 class CompletedFiler(TaskFilter):
     def match(self, task: Task) -> bool:
         return task.completed
+
+
+# Just to show:
+# The following approach would violate the Open-Closed-Principle,
+# because in order to extend the functionality (i.e. when adding
+# a new filter-type and it's filtering-condition), a new if-statement
+# would have to be added, and thus the 'match'-function would need
+# to be modified (i.e. the 'match'-function would be open for extension,
+# but would NOT be closed for modification!)
+"""
+class Filter:
+    def match(task, concreteFilter) -> bool:
+        if concreteFilter == "FilterA":
+            return task.completed()
+        if concreteFilter == "FilterB":
+            return task.category == "SomeCategory"
+"""
+        
         
